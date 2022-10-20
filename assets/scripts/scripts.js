e041f6bb9cfbb2282be0ee4adb180ee2e3d6ac99
Nathan Ryan <rockymountainshredder@gmail.com>
2022-10-19 22:40:10 -0600

    Added printBooks function to loop through myLibrary and display to page

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,34 @@
+
+// const mainDiv = document.getElementById('main-div');
+const main = document.querySelector('main');
+const displayBooks = document.getElementById('display-books');
+
+let myLibrary = [
+    { title: 'Art of War', author: 'Sun Tzu' },
+    { title: '1984', author: 'George Orwell' },
+    { title: 'River God', author: 'Wilbur Smith' },
+    { title: 'Measure for Measure', author: 'William Shakespeare' },
+];
+
+function Book() {
+    // Constructor
+}
+
+function addBookToLibrary() {
+    // TODO
+}
+
+// Function to loop through the array and display each book on the page..
+const printBooks = myLibrary.filter(function(book) {
+    // Create a div element
+    let div = document.createElement('div');
+    // Use template literal to add book properties to created divs text content
+    div.textContent = `Title: ${book.title}, Author: ${book.author}.`;
+    // Append book information to main div
+    main.appendChild(div);
+    // mainDiv.appendChild(div);
+});
+
+// displayBooks.textContent = `Title: ${book.title}, Author: ${book.author}.`;
+// mainDiv.appendChild(displayBooks);
+// console.log(printBooks);
